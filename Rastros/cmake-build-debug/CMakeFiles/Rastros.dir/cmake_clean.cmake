20114854c2cbdf1c753c1948136fdac0e41c6211
David Alexandre Ferreira Duarte A93253 PL5 <a93253@alunos.uminho.pt>
2020-03-12 19:27:31 +0000

    Adicionado a pasta com os modulos do jogo Rastros

---
+++ Rastros/cmake-build-debug/CMakeFiles/Rastros.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rastros.dir/main.c.o"
+  "Rastros"
+  "Rastros.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Rastros.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
