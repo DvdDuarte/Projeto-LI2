84c0adc2bb964f8d06fb8f6aacfb8ad85cddaa57
David Alexandre Ferreira Duarte A93253 PL5 <a93253@alunos.uminho.pt>
2020-04-05 19:36:43 +0100

    Funcao para o comando pos prototipo

--- Rastros/Camadas de dados/buscaPosicao.c
+++ Rastros/Camadas de dados/buscaPosicao.c
@@ -8,50 +8,26 @@
 
 ESTADO posicao (int pos, ESTADO *e) {
 
+    ESTADO *estadonovo;
+    int i;
+    pos = pos - 1;
 
+    if (pos < 0 || pos >= e->num_jogadas) return 0;
 
+    inicializar_estado(estadonovo);
 
+    for (i = 0; i <= pos; i++){
 
+        estadonovo -> jogadas[i] = e -> jogadas[i]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   // ESTADO *estadonovo;
-  //  int i;
-  //  pos = pos - 1;
-
-   // if (pos < 0 || pos >= e->num_jogadas) return 0;
-
-   // inicializar_estado(estadonovo);
-
-    //for (i = 0; i <= pos; i++){
-
-     //   estadonovo -> jogadas[i] = e -> jogadas[i]
-
-   // }
+    }
 
     //Fazer condicao para ver qual foi o ultimo jogador para atualizar a ultima jogada em concordancia
     //estadonovo -> ultima_jogada.coluna = e -> jogadas.jogador1.coluna;
-    //estadonovo -> ultima_jogada.linha = e -> jogadas.jogador1.linha;
+   // estadonovo -> ultima_jogada.linha = e -> jogadas.jogador1.linha;
+    estadonovo -> ultima_jogada.coluna = e -> jogadas.jogador2.coluna;
+    estadonovo -> ultima_jogada.linha = e -> jogadas.jogador2.linha;
 
-   // return estadonovo;
+    return estadonovo;
 
 }
